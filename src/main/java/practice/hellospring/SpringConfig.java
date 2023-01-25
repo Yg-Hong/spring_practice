8c576d2d669f11c93c20f1e671b3923a561a3b06
Yg-Hong <lion0077v@gmail.com>
2023-01-25 18:50:36 +0900

    [day 5] spring DB 접근 기술(JPA)

--- src/main/java/practice/hellospring/SpringConfig.java
+++ src/main/java/practice/hellospring/SpringConfig.java
@@ -1,23 +1,31 @@
 package practice.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import practice.hellospring.repository.JpaMemberRepository;
 import practice.hellospring.repository.MemberRepository;
-import practice.hellospring.repository.MemoryMemberRepository;
-import practice.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
-    /*
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(@Qualifier("memoryMemberRepository") MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }*/
+    /*
 
+    @Bean
+    public MembenrService memberService(){
+        return new MemberService(memberRepository());
+    }
+    */
+    //@Bean
+    //public JpaMemberRepository memberRepository(){
+    //    return new JpaMemberRepository(em);
+    //}
 }
