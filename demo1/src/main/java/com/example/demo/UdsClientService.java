935b3f39cea28166c186d1c8be039810d715597a
Yg-Hong <lion0077v@gmail.com>
2025-02-25 16:47:50 +0900

    test

--- demo1/src/main/java/com/example/demo/UdsClientService.java
+++ demo1/src/main/java/com/example/demo/UdsClientService.java
@@ -15,7 +15,7 @@ public class UdsClientService {
     private static final int BUFFER_SIZE = 2048;
 
     public String sendPacket(String sendMsg, int timeout) {
-        String sessionId = "0"; // 실제로는 세션 기반 ID를 받아야 함.
+        String sessionId = "0";
         File clientSockFile = new File(CLIENT_SOCK_FILE + sessionId);
         File serverSockFile = new File(SERVER_SOCK_FILE);
 
@@ -24,21 +24,32 @@ public class UdsClientService {
             clientSockFile.delete();
         }
 
-        // 서버 소켓 파일이 존재하는지 확인
+        // 서버 소켓 파일 존재 확인
         if (!serverSockFile.exists()) {
-            return null; // 서버가 실행 중이지 않음
+            System.err.println("Error: Server socket file does not exist: " + SERVER_SOCK_FILE);
+            return null;
+        }
+
+        // 서버 소켓 파일이 유효한 소켓인지 확인
+        if (!serverSockFile.isFile()) {
+            System.err.println(
+                "Error: " + SERVER_SOCK_FILE + " is not a valid UNIX domain socket.");
+            return null;
         }
 
         try (AFUNIXDatagramSocket socket = AFUNIXDatagramSocket.newInstance()) {
             // 클라이언트 소켓 바인딩
             socket.bind(AFUNIXSocketAddress.of(clientSockFile));
-            // 타임아웃 설정
             socket.setSoTimeout(timeout * 1000);
 
             // 서버로 메시지 전송
             byte[] sendData = sendMsg.getBytes();
+            AFUNIXSocketAddress serverAddress = AFUNIXSocketAddress.of(serverSockFile);
+
+            System.out.println("Sending message to: " + serverAddress.getPath());
+
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
-                AFUNIXSocketAddress.of(serverSockFile));
+                serverAddress);
             socket.send(sendPacket);
 
             // 서버 응답 수신
@@ -46,11 +57,11 @@ public class UdsClientService {
             DatagramPacket recvPacket = new DatagramPacket(recvData, recvData.length);
             socket.receive(recvPacket);
 
-            // 응답을 문자열로 변환하여 반환
             return new String(recvPacket.getData(), 0, recvPacket.getLength());
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
+
 }
