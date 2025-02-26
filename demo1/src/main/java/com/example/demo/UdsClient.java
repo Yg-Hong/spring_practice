af94e377b314ff7f6d86c32df0c882aa2cb49620
Yg-Hong <lion0077v@gmail.com>
2025-02-26 17:35:20 +0900

    test

--- demo1/src/main/java/com/example/demo/UdsClient.java
+++ demo1/src/main/java/com/example/demo/UdsClient.java
@@ -1,69 +1,124 @@
 package com.example.demo;
 
-import java.io.BufferedReader;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.net.SocketException;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
 import org.newsclub.net.unix.AFSocketAddress;
 import org.newsclub.net.unix.AFUNIXSocket;
 import org.newsclub.net.unix.AFUNIXSocketAddress;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class UdsClient {
 
-    private final File clientSockFile;
-    private final File serverSockFile;
+    private final String SOCKET_PATH;
+    private final int BUFFER_SIZE;
+    private AFUNIXSocket sock;
+    private OutputStream out;
+    private InputStream in;
 
     public UdsClient() {
-        this.clientSockFile = new File("/tmp/.webc_c_0");
-        this.serverSockFile = new File("/tmp/.webc_server");
+        this.SOCKET_PATH = "/tmp/.webc_server";
+        this.BUFFER_SIZE = 4096;
     }
 
-    public String sendPacket(String message, int timeoutSec) {
-        if (!serverSockFile.exists()) {
-            return null; // 리턴 조건: 서버 소켓 파일이 없으면 요청을 보낼 수 없음
+    @PostConstruct
+    public void connect() throws IOException {
+        SocketAddress endpoint = getSocketAddress(SOCKET_PATH);
+        sock = AFUNIXSocket.connectTo(AFUNIXSocketAddress.of(endpoint));
+        out = sock.getOutputStream();
+        in = sock.getInputStream();
+
+        log.info("UDS Connected Completely to {}", endpoint);
+    }
+
+    @PreDestroy
+    public void disconnect() throws IOException {
+        if (sock != null && !sock.isClosed()) {
+            sock.close();
+            log.info("Disconnected from server on " + SOCKET_PATH);
+        }
+    }
+
+    public boolean isValid() {
+        if (sock == null) {
+            return false;
+        } else if (!sock.isConnected()) {
+            return false;
         }
 
-        // 기존 소켓 파일이 존재하면 삭제
-        if (clientSockFile.exists()) {
-            clientSockFile.delete();
+        return true;
+    }
+
+    public void reconnect() throws IOException {
+        if (this.isValid()) {
+            return;
         }
 
-        try (AFUNIXSocket socket = AFUNIXSocket.newInstance()) {
-            SocketAddress serverAddress = AFUNIXSocketAddress.of(serverSockFile);
-            socket.connect(serverAddress);
+        SocketAddress endpoint = getSocketAddress(SOCKET_PATH);
+        sock = AFUNIXSocket.connectTo(AFUNIXSocketAddress.of(endpoint));
+        out = sock.getOutputStream();
+        in = sock.getInputStream();
+    }
 
-            // 송신
-            OutputStream out = socket.getOutputStream();
-            out.write(message.getBytes(StandardCharsets.UTF_8));
-            out.flush();
+    public String sendMessage(String message) throws IOException {
+        if (sock == null || !sock.isConnected()) {
+            log.info("UDS NOT CONNECTED!!!");
+            throw new IOException("UDS Not Connected");
+        }
+        log.info("Now writing string({}) to the server...", message);
+        out.write(message.getBytes());
+        out.flush();
 
-            // 수신
-            InputStream inputStream = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(
-                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            String response = reader.readLine();
-
-            System.out.printf("response : %s\n", response);
-
-            return response != null ? response : "No response";
-        } catch (SocketException e) {
-            e.printStackTrace();
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int numRead = in.read(buffer);
+        if (numRead > 0) {
+            return new String(buffer, 0, numRead);
+        } else {
             return null;
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        return "echo";
     }
+
+//    public String sendPacket(String message, int timeoutSec) {
+//        if (!serverSockFile.exists()) {
+//            return null; // 리턴 조건: 서버 소켓 파일이 없으면 요청을 보낼 수 없음
+//        }
+//
+//        try (AFUNIXSocket socket = AFUNIXSocket.newInstance()) {
+//            SocketAddress serverAddress = AFUNIXSocketAddress.of(serverSockFile);
+//            socket.connect(serverAddress);
+//
+//            // 송신
+//            OutputStream out = socket.getOutputStream();
+//            out.write(message.getBytes(StandardCharsets.UTF_8));
+//            out.flush();
+//
+//            // 수신
+//            InputStream inputStream = socket.getInputStream();
+//            BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+//            String response = reader.readLine();
+//
+//            System.out.printf("response : %s\n", response);
+//
+//            return response != null ? response : "No response";
+//        } catch (SocketException e) {
+//            e.printStackTrace();
+//            return null;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return "echo";
+//    }
 
     private SocketAddress getSocketAddress(String socketName) throws IOException {
         if (socketName.startsWith("file:")) {
