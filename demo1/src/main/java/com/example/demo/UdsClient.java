9f000fee51a56643b4f9e91c40c8ab12754c3625
Yg-Hong <lion0077v@gmail.com>
2025-02-26 15:23:49 +0900

    test

--- demo1/src/main/java/com/example/demo/UdsClient.java
+++ demo1/src/main/java/com/example/demo/UdsClient.java
@@ -2,30 +2,45 @@ package com.example.demo;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.UnixDomainSocketAddress;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.file.Path;
+import java.net.SocketException;
+import org.newsclub.net.unix.AFUNIXDatagramSocket;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
 import org.springframework.stereotype.Component;
 
 @Component
 //@RequiredArgsConstructor
 public class UdsClient {
 
-    public String sendPacket(String sendMsg, int timeoutSec) {
-        // 소켓 파일 경로 지정
-        Path socketPath = Path.of("/tmp/.webc_server");
+    private final File sendSockFile;
+    private final File recvSockFile;
 
-        try (ServerSocketChannel serverSocket = ServerSocketChannel.open()) {
-            // 유닉스 도메인 소켓 주소 생성
-            UnixDomainSocketAddress address = UnixDomainSocketAddress.of(socketPath);
-            serverSocket.bind(address);
-            // 주소 정보 출력
-            System.out.println("Unix Domain Socket Address: " + address.getPath());
+    public UdsClient() {
+        this.sendSockFile = new File("/tmp/.webc_c_0");
+        this.recvSockFile = new File("/tmp/.webc_server");
+    }
+
+    public String sendPacket(String message, int timeoutSec) {
+        if (!recvSockFile.exists()) {
+            return null; // 리턴 조건: 서버 소켓 파일이 없으면 요청을 보낼 수 없음
+        }
+
+        // 기존 소켓 파일이 존재하면 삭제
+        if (sendSockFile.exists()) {
+            sendSockFile.delete();
+        }
+
+        try (AFUNIXDatagramSocket socket = AFUNIXDatagramSocket.newInstance()) {
+            socket.setSoTimeout(timeoutSec * 1000);
+
+            AFUNIXSocketAddress sendAddress = new AFUNIXSocketAddress(sendSockFile);
+            socket.bind(sendAddress);
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+            return null;
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return "echo";
     }
 
 }
