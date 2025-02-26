2fe04219b5d765072c3ac3c435718f26102e64ba
Yg-Hong <lion0077v@gmail.com>
2025-02-26 15:14:52 +0900

    test

--- demo1/src/main/java/com/example/demo/UdsClient.java
+++ demo1/src/main/java/com/example/demo/UdsClient.java
@@ -5,32 +5,15 @@ import java.io.IOException;
 import java.net.UnixDomainSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.file.Path;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class UdsClient {
 
-    private final UdsClientConfig config;
-    private final File sendSockFile;
-    private final File recvSockFile;
-
-    public UdsClient(String sessionId) {
-        this.sendSockFile = new File("/tmp/.webc_c_" + (sessionId.isEmpty() ? "0" : sessionId));
-        this.recvSockFile = new File("/tmp/.webc_server");
-    }
-
     public String sendPacket(String sendMsg, int timeoutSec) {
-        if (!recvSockFile.exists()) {
-            return null; // 리턴 조건: 서버 소켓 파일이 없으면 요청을 보낼 수 없음
-        }
-
+        // 소켓 파일 경로 지정
         Path socketPath = Path.of("/tmp/.webc_server");
-        // 기존 소켓 파일이 존재하면 삭제
-        if (sendSockFile.exists()) {
-            sendSockFile.delete();
-        }
 
         try (ServerSocketChannel serverSocket = ServerSocketChannel.open()) {
             // 유닉스 도메인 소켓 주소 생성
