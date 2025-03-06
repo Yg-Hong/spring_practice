06cb0a1e4ca81891abf7dd73e83118742e2b8421
Yg-Hong <lion0077v@gmail.com>
2025-03-06 18:37:19 +0900

    test

--- demo1/src/main/java/com/example/demo/FirewallController.java
+++ demo1/src/main/java/com/example/demo/FirewallController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("firewall")
+@RequestMapping("/firewall")
 public class FirewallController {
 
     private final FirewallService firewallUdsService;
